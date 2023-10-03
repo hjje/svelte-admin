aefc046daa4ed8a5b41ad6462ae21d8e5c19efe1
hjje <hjjeon5435@gmail.com>
2023-10-03 15:21:50 +0900

    mysql

---
+++ src/routes/api/+server.js
@@ -0,0 +1,20 @@
+import { json } from "@sveltejs/kit";
+import { mysqlconnFn } from "$lib/db/mysql";
+
+export function GET(){
+    return json("GENIUS@@@@@@@@");
+}
+
+export async function POST({ request }) {
+  const { st } = await request.json();
+  let mysqlconn = await mysqlconnFn();
+  let results = await mysqlconn
+    .query("SELECT * FROM states where state = '" + st + "'")
+    .then(function ([rows, fields]) {
+      //     console.log("Got this far!!");
+      //     console.log(rows);
+      return rows;
+    });
+
+  return json(results);
+}
